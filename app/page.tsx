b3ce39a6b5af8e045495ac7f61111f0b489906f5
Dorian Le <dorianbgle@icloud.com>
2024-05-19 11:46:16 +1000

    Supabase and ShadcnUI integrations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import supabase from "@/lib/supabase";
+import { useState, useEffect } from "react";
+
+type MenuItem = {
+  id: number;
+  name: string;
+}
 
 export default function Home() {
+  
+  const [menu, setMenu] = useState<MenuItem[]>([])
+  
   return (
-    <main className="">
-    <div>Penis</div>
-    <h2>Test</h2>      
+    <main className="w-full h-full">
+    <div className=" ml-10 w-full ustify-center items-center">Penis</div>
+    
+    {menu.map((m) => (
+      <h1 key={m.id}>{m.name}</h1>
+    ))}
+
     </main>
   );
 }
