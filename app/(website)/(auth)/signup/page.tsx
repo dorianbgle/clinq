641d71329b7ddc97b4e26c60e4c18aa4309ea4b8
Dorian Le <dorianbgle@icloud.com>
2024-09-24 20:31:48 +1000

    Trial #62

--- app/(website)/(auth)/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/pricing-components/(action-component)/AuthForm";
-
-const Page = () => {
-  return (
-    <AuthForm type="signup"/>
-  )
-};
-
-export default Page
