1f89dec6baa4efa57ea822a70ab92d108ec9f757
Dorian Le <dorianbgle@icloud.com>
2024-09-29 15:23:34 +1000

    Trial #75

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,11 @@ import { FooterCTA } from "./(website)/components/footer-cta";
 import { Footer } from "./(website)/components/footer";
 import { usePathname } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { useEffect } from "react";
-import { toast, Toaster } from 'react-hot-toast';
+import { toast, Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
@@ -26,10 +26,13 @@ export default function RootLayout({
     const showToast = localStorage.getItem("signOutToast");
     if (showToast === "true") {
       // Show the success toast
-      toast.success('You have successfully signed out.', {   style: {
-        borderRadius: '10px',
-        background: '#333',
-        color: '#000000'}});
+      toast.success("You have successfully signed out.", {
+        style: {
+          borderRadius: "10px",
+          background: "#000000",
+          color: "#FFFFFF",
+        },
+      });
       // Remove the flag from local storage after displaying the toast
       localStorage.removeItem("signOutToast");
     }
@@ -45,20 +48,23 @@ export default function RootLayout({
       >
         {pathname === "/" ? (
           <main className="container mx-auto px-4 overflow-hidden md:overflow-visible">
-            <Analytics/>    
+            <Analytics />
             <SpeedInsights />
             <Header />
             {children}
             <FooterCTA />
             <Footer />
             <Toaster position="bottom-right" reverseOrder={false} />
-           
           </main>
         ) : (
-          <><SessionProvider>{children}<Toaster position="top-center" reverseOrder={false} /></SessionProvider></>
+          <>
+            <SessionProvider>
+              {children}
+              <Toaster position="top-center" reverseOrder={false} />
+            </SessionProvider>
+          </>
         )}
       </body>
-
     </html>
   );
 }
