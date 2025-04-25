3723f0156608f376e663f43e12bc033415f3f96b
Dorian Le <dorianle@Dorians-MacBook-Pro.local>
2025-04-25 14:23:53 +1000

    Trial Commit - Altered copy

--- app/(website)/(homepage)/checklistguide/page.tsx
+++ app/(website)/(homepage)/checklistguide/page.tsx
@@ -1,11 +1,10 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Vault",
-    description:
-      "Don’t waste time searching through old emails and random folders. Keep all your contracts, agreements and more safe in one place.",
-  };
-  
+  title: "ClinQ - Checklists",
+  description:
+    "Don’t waste time searching through old emails and random folders. Keep all your contracts, agreements and more safe in one place.",
+};
 
 const Page = () => {
   return (
@@ -21,51 +20,26 @@ const Page = () => {
 
         <div className="flex items-center flex-col text-center relative">
           <p className="text-lg mt-4 max-w-[600px]">
-            Don’t waste time searching through old emails and random folders.
-            Keep all your contracts, agreements and more safe in one place.
+            Don’t waste time making checklists to practise. Spend more time
+            practising our pre-made checklists.
           </p>
         </div>
       </div>
-
-      {/* <Image src={Vault} quality={100} alt="Vault" /> */}
 
       <div className="flex items-center flex-col text-center relative mt-28">
         <div className="max-w-[600px]">
           <h4 className="font-medium text-xl md:text-2xl mb-4">
-            All your files in one place
+            All the in one place
           </h4>
           <p className="text-[#878787] text-sm">
-            Gather all your business files you have laying around and have them
-            in one place. Upload quickly and be able to share them with whomever
-            you want.
+            All the checklists you need to practise are in one place. Don&apos;t
+            waste time formating, finding information or even making them. Just
+            pick the one you need and start practising.
           </p>
-        </div>
-
-        {/* <Image
-          src={Files}
-          quality={100}
-          alt="Files"
-          className="mt-10 max-w-[834px] w-full"
-        /> */}
-
-        <div className="mt-32 max-w-[550px]">
-          <h4 className="font-medium text-xl md:text-2xl mb-4">
-            Use assistant to search
-          </h4>
-          <p className="text-[#878787] text-sm mb-10">
-            Use the assistant to search for your files or even within your
-            files. Say you want to find that old contract but can’t remember
-            which client it was for, just search for details around it and the
-            assistant will find it for you.
-          </p>
-        </div>
-
-        <div className="text-left scale-[0.45] md:scale-100 -mt-20 md:mt-0">
-          {/* <Assistant /> */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
